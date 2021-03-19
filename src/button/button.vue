916eeb02d1181e63598fa9d5dd93bee4e480c12b
comfuture <comfuture@iplateia.com>
2021-03-19 14:26:49 +0900

    implement very basic button component

--- src/button/button.vue
+++ src/button/button.vue
@@ -1,18 +1,23 @@
 <template>
-  <button>{{ label }}</button>
+  <button :class="{primary}">
+    <slot></slot>
+  </button>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import "../base.css";
 
 @Component
 export default class UiButton extends Vue {
     @Prop()
-    private foo: string = "";
+    private primary: boolean = false;
 }
 </script>
 <style>
-button.ui {
-  @apply bg-gray-500;
+button.ui, .ui.button {
+  @apply bg-gray-500 px-2 py-4 rounded font-semibold;
+
+  &.primary {
+    @apply bg-blue-500;
+  }
 }
 </style>
